26ec579e91af2e5ac8ab37b57e6adbe5e914c097
daniar <daniar1986@mail.ru>
2015-12-27 16:47:57 +0200

    Moving Snake

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -55,6 +55,12 @@ namespace Snake
             Console.Write(sym);
         }
 
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
+
         public override string ToString()
         {
             return x + ", " + y + ", " + sym;
