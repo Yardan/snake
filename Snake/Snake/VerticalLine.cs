f1cadea8675e9aafaa5e8c44aa590fabe0f3fa75
daniar <daniar1986@mail.ru>
2015-12-27 02:20:37 +0200

    Inheritance

--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        List<Point> pList;
-
         public VerticalLine(int yTop, int yBottom, int x, char sym)
         {
             pList = new List<Point>();
@@ -18,14 +16,6 @@ namespace Snake
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
-            }
-        }
-
-        public void Draw()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
             }
         }
     }
